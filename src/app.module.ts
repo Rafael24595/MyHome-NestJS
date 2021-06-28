04430b3b637ebdec19f54c0e8fae99733c32eca9
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2021-06-29 00:44:46 +0200

    [FEATURE] Implementación NestJS

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from './modules/user/user.module';
+
+const mongoUri = "mongodb://localhost:27017/myhome";
+
+@Module({
+  imports: [MongooseModule.forRoot(mongoUri), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

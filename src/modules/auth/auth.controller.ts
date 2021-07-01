ccb881606931f63ab7c4831c267bdc4b53c9ac82
Rafael Ruiz <rafael.ruiz@sisnet360.com>
2021-07-01 09:34:57 +0200

    [FEATURE] Añadida estrategia de autorización con JWT

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { User } from '../user/interfaces/user.interface';
-import { LocalAuthGuard } from './strategies/guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authService:AuthService){}
+    
     @UseGuards(LocalAuthGuard)
     @Post('auth/login')
     async login(@Request() req:{user:User}){
-        return req.user;
+        return this.authService.login(req.user);
     }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from "../user/user.module";
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './variables/variables';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports:[UserModule],
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports:[
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: {expiresIn: '60s'}
+    })
+  ],
+  controllers: [AuthController, LocalStrategy],
+  providers: [AuthService, JwtModule]
 })
 export class AuthModule {}

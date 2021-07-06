326766921293b2fd39ba214c49e4c7979fa47f45
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2021-07-06 22:16:49 +0200

    [FEATURE] Añadida la estructura de Angular al proyecto

---
+++ my-home-angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { user_config } from '../utils/variables/Globals';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  Globals_User = user_config;
+}
---
+++ my-home-angular/src/utils/variables/Globals.ts
@@ -0,0 +1,6 @@
+import { User } from "src/classes/User";
+
+export let user_config: {user: User | undefined, validUser: boolean} = {
+    user: undefined,
+    validUser: false
+};

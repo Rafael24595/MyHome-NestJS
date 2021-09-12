68b638fca900be74b947469866dc6b54fdd39685
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2021-09-12 22:09:32 +0200

    [FEATURE] Actualizadas variables de modal-form.

--- .my-home-angular/src/app/components/modal-components/modal-base.component.ts
+++ .my-home-angular/src/app/components/modal-components/modal-base.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Modal } from 'src/classes/Modal';
+import { ModalForm } from 'src/classes/ModalForm';
 
-export let ModalStatus: {showModal: boolean, modalType: string, options: boolean, modalDataSimple: Modal} = {
+export let ModalStatus: {showModal: boolean, modalType: string, options: boolean, modalDataSimple: Modal, modalDataForm: ModalForm} = {
   showModal: false,
   modalType: '',
   options: true,
   modalDataSimple: Modal.getEmptyModal(),
+  modalDataForm: ModalForm.getEmptyModal()
 }
 
 @Component({
@@ -17,7 +19,8 @@ export class ModalBaseComponent implements OnInit {
 
   modal = ModalStatus;
   static modalTypes = {
-    simple: 'simple'
+    simple: 'simple',
+    form: 'form'
   }
   
   constructor() { }
@@ -33,13 +36,18 @@ export class ModalBaseComponent implements OnInit {
     ModalStatus.showModal = false;
     ModalStatus.modalType = '';
     ModalStatus.modalDataSimple = Modal.getEmptyModal();
+    ModalStatus.modalDataForm = ModalForm.getEmptyModal();
   }
 
-  static openModal(modal: Modal): void{
+  static openModal(modal: Modal | ModalForm): void{
     ModalStatus.showModal = true;
     if(modal instanceof Modal){
       ModalStatus.modalDataSimple = modal;
       ModalStatus.modalType = ModalBaseComponent.modalTypes.simple;
+    }
+    if(modal instanceof ModalForm){
+      ModalStatus.modalDataForm = modal;
+      ModalStatus.modalType = ModalBaseComponent.modalTypes.form;
     }
   }
 }
